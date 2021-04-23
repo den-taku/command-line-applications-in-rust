84bcf6a9bdc036560f37eaf0f54347b562ad5dd7
DenTaku <dentaku1412@gmail.com>
2021-04-23 11:03:38 +0900

    Add structopt

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,15 @@
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+}
+
+/// Search for a patter in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
