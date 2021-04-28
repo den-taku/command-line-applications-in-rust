0241f8b0fc7e9aae6133777a3ef0311c8190699e
DenTaku <dentaku1412@gmail.com>
2021-04-29 01:19:08 +0900

    Learn how to do integration test

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,32 +1,5 @@
-use structopt::StructOpt;
 use anyhow::{Context, Result};
-use std::io::{self, Write};
-use log::{info, warn};
-
-fn main() -> Result<()> {
-    // let pb = indicatif::ProgressBar::new(100);
-    // let mut i = 0;
-    // for i in 0..100 {
-        // do_hard_work();
-        // pb.println(format!("[+] finished #{}", i));
-        // pb.inc(1);
-    // }
-    // pb.finish_with_message("done");
-    // env_logger::init();
-    // info!("starting up");
-    // warn!("oops, nothing implemented!");
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("could not read file `{:?}`", &args.path))?;
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            writeln!(handle, "{}", line)?;
-        }
-    }
-    Ok(())
-}
+use structopt::StructOpt;
 
 /// Search for a patter in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -38,10 +11,12 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-// fn do_hard_work() -> i32 {
-//     let mut i = 0;
-//     for j in 0..1000000 {
-//         i += 1;
-//     }
-//     i
-// }
+fn main() -> Result<()> {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{:?}`", &args.path))?;
+
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout())?;
+
+    Ok(())
+}
---
+++ grrs/tests/cli.rs
@@ -0,0 +1,45 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::io::Write;
+use std::process::Command;
+use tempfile::NamedTempFile;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
+
+#[test]
+fn passing_an_emmpty_string_as_pattern() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg(file.path());
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("The following required arguments were not provided"));
+
+    Ok(())
+}
