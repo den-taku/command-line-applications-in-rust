a599b95d9dc65c93f7068d96f97244bfed944bc8
DenTaku <dentaku1412@gmail.com>
2021-04-30 00:59:04 +0900

    Manage output when panic

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,7 +1,10 @@
 use anyhow::{Context, Result};
 use structopt::StructOpt;
+use human_panic::setup_panic;
 
 fn main() -> Result<()> {
+    setup_panic!();
+
     let args = grrs::Cli::from_args();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{:?}`", &args.path))?;
